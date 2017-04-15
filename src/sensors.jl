bf94b274af954008f73918c0d918a36763a32e49
Jerome LE NY <jerome.le-ny@polymtl.ca>
2017-04-15 17:27:51 -0400

    Fixed mistakes in comment

--- src/sensors.jl
+++ src/sensors.jl
@@ -1,13 +1,13 @@
-```
-    simulateBrownianMotion(PSD, T; dt = T*1e-3)
+"""
+    simulateBrownianMotion(PSD, T; dt = T * 1e-3)
 
 returns sample paths of Brownian motion, dB/dt = w, with w white Gaussian
 noise with power spectral density psd. T is the time horizon.
 dt is the size of the dicrete-time steps used in the simulation.
-```
+"""
 function simulateBrownianMotion(PSD, T; dt = T*1e-3)
   nsteps = Int(ceil(T/dt))
-  res = sqrt(PSD) * randn(nsteps+1)
+  res = sqrt(PSD)*randn(nsteps+1)
   res[1] = 0
   return cumsum(res)
 end
